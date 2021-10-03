067a52201fd4e88c9e9c9ae27e95131ae2ea9b6f
Surinder75 <91563453+Surinder75@users.noreply.github.com>
2021-10-03 09:08:10 -0400

    Update Morning.java
    Lunch Added By Surinder

--- src/com/sundaypackage/Morning.java
+++ src/com/sundaypackage/Morning.java
@@ -4,6 +4,7 @@ public class Morning {
 	public static void main(String[] args) {
 		System.out.println("Beautiful Morning");
 		System.out.println("Eating my breafast");
+		System.out.println(("Preparing Lunch");
 	}
 
 }
