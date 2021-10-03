62beb2e2c8767ed67325709b3ba2239c83d6d025
tanvirrandhawa <tanvirrandhawa@tanvirs-mbp.fios-router.home>
2021-10-03 09:05:33 -0400

    breakfast added by Surinder

--- src/com/sundaypackage/Morning.java
+++ src/com/sundaypackage/Morning.java
@@ -3,6 +3,7 @@ package com.sundaypackage;
 public class Morning {
 	public static void main(String[] args) {
 		System.out.println("Beautiful Morning");
+		System.out.println("Eating my breafast");
 	}
 
 }
